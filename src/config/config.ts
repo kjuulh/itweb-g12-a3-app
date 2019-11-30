c7605298fb1ffd9d64351d5bb0d0bf1299bdf76c
hermansen <hermansendev@gmail.com>
2019-11-30 22:03:34 +0100

    Add backend url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,8 @@
 export class Config {
   public static isProduction = process.env.NODE_ENV;
-  public static serverUrl =
-    Config.isProduction == 'production'
-      ? 'https://itweb-g12-a3-api.herokuapp.com/'
-      : 'http://localhost:8080';
+  public static serverUrl = process.env.BACKEND_URL
+    ? process.env.BACKEND_URL
+    : Config.isProduction == 'production'
+    ? 'https://itweb-g12-a3-api.herokuapp.com/'
+    : 'http://localhost:8080';
 }
