1e2ebd33f110eddf81dab39d0a0953716ff06b9f
hermansen <hermansendev@gmail.com>
2019-11-30 22:15:34 +0100

    add staging url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,9 @@
 export class Config {
-  public static isProduction = process.env.NODE_ENV;
-  public static serverUrl = process.env.BACKEND_URL
-    ? process.env.BACKEND_URL
-    : Config.isProduction == 'production'
-    ? 'https://itweb-g12-a3-api.herokuapp.com/'
-    : 'http://localhost:8080';
+  public static environment = process.env.ENVIRONMENT;
+  public static serverUrl =
+    Config.environment == 'production'
+      ? 'https://itweb-g12-a3-app.herokuapp.com'
+      : Config.environment == 'staging'
+      ? 'https://itweb-g12-a3-app-staging.herokuapp.com'
+      : 'http://localhost:8080';
 }
