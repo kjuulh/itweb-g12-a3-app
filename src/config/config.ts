c7d90abee98fea07b2823bdd20bdb91811d3c436
hermansen <hermansendev@gmail.com>
2019-12-01 09:05:30 +0100

    config

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,8 @@ export class Config {
   public static environment = process.env.REACT_APP_ENVIRONMENT;
   public static serverUrl =
     Config.environment == 'production'
-      ? 'https://itweb-g12-a3-app.herokuapp.com'
+      ? 'https://itweb-g12-a3-api.herokuapp.com'
       : Config.environment == 'staging'
-      ? 'https://itweb-g12-a3-app-staging.herokuapp.com'
+      ? 'https://itweb-g12-a3-api-staging.herokuapp.com'
       : 'http://localhost:8080';
 }
