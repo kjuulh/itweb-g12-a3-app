f22bf7a177a6bf958735457ed9f316161b3097ff
hermansen <hermansendev@gmail.com>
2019-12-06 11:42:56 +0100

    Add staging

---
+++ build/precache-manifest.5d2277f754a86d56015c99ad9e179f31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ad3a5468f81f19d9273e0c91093430",
+    "url": "/DualNBack/index.html"
+  },
+  {
+    "revision": "b5bc87ea6de5f836c0e8",
+    "url": "/DualNBack/static/css/main.514a5c74.chunk.css"
+  },
+  {
+    "revision": "0a71bd6fac20bb766c8e",
+    "url": "/DualNBack/static/js/2.f465ef28.chunk.js"
+  },
+  {
+    "revision": "b5bc87ea6de5f836c0e8",
+    "url": "/DualNBack/static/js/main.7e967992.chunk.js"
+  },
+  {
+    "revision": "20ae2143e53e8f4b4f26",
+    "url": "/DualNBack/static/js/runtime-main.6bdb25b8.js"
+  }
+]);
